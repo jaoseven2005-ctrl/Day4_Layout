defea26ca9ec50196dfff4d3e4bc7a8c0ee00ec0
jaoseven2005 <EMAIL_BAN_DANG_KY_GITHUB>
2025-09-18 21:11:45 +0700

    Initial commit

---
+++ Day4_layout/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Day4_layout.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day4_layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
